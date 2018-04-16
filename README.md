87ffb2fb87ffd48110015c61e6e36100edc009a6
Olekciy <0lekciy4@gmail.com>
2018-04-16 10:39:14 +0300

    Realized: search github trending(stars, open issues, url)

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
 # Github Trends
 
-[TODO. There will be project description]
+the script finds all the repositories created in the last week,
+selects 20 with the largest number of stars,
+and for each counts the number of open issues. And displays them
+in the terminal along with the links.
+
+# Example run
+
+Open the terminal, go to the directory where the file is located and
+run "python github_trending.py", if python 3 as not default try
+"python3 github_trending.py"
+
+```bash
+$ python github_trending.py
+Stars: 2526
+Open issues: 6
+Repo url: https://github.com/luanfujun/deep-painterly-harmonization
+Stars: 678
+Open issues: 6
+Repo url: https://github.com/hannob/snallygaster
+Stars: 647
+Open issues: 1
+Repo url: https://github.com/itchyny/bed
+
+. . .
+
+```
+
+# How to install
+
+Python 3 should be already installed. Then use pip (or pip3 if there
+is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,45 @@
+from datetime import date, timedelta
+
+import requests
+
+TOP_SIZE = 20
+GITHUB_API_URL = 'https://api.github.com'
+GITHUB_API_SEARCH_QUERY = 'search/repositories'
+GITHUB_API_CREATED = 'q=created'
+REPOSITORIES_FROM_REQUEST = 'items'
+GITHUB_STARS = 'stargazers_count'
+GITHUB_OPEN_ISSUES = 'open_issues_count'
+GITHUB_REPO_URL = 'html_url'
+
+
+def get_past_date(days=7):
+    now = date.today()
+    date_for_search = now - timedelta(days=days)
+    return date_for_search
 
 
 def get_trending_repositories(top_size):
-    pass
+    last_week = get_past_date()
+    request = requests.get(
+        '{}/{}?{}:>={}'.format(
+            GITHUB_API_URL,
+            GITHUB_API_SEARCH_QUERY,
+            GITHUB_API_CREATED,
+            last_week,
+        ),
+    )
+    all_repositories = request.json()[REPOSITORIES_FROM_REQUEST]
+    all_repositories.sort(
+        key=lambda item: item[GITHUB_STARS],
+        reverse=True,
+    )
+    trending_repositories = all_repositories[:top_size]
+    return trending_repositories
 
-
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
 
 if __name__ == '__main__':
-    pass
+    trending_repositories = get_trending_repositories(TOP_SIZE)
+    for repo in trending_repositories:
+        print('Stars: {}'.format(repo[GITHUB_STARS]))
+        print('Open issues: {}'.format(repo[GITHUB_OPEN_ISSUES]))
+        print('Repo url: {}'.format(repo[GITHUB_REPO_URL]))
